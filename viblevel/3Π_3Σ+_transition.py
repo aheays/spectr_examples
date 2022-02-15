39fd5274eb1415c698e945806c61f1ae5dbfacc4
Alan Heays <aheays@fastmail.com>
2022-02-15 12:54:58 +0100

    updated README

--- viblevel/3Π_3Σ+_transition.py
+++ viblevel/3Π_3Σ+_transition.py
@@ -1,33 +1,27 @@
-from spectr import *
+"""Compute level energies from molecular constants and combine into a
+line list."""
+
+from spectr.env import *
+
+## level data
+vlevel = viblevel.Level(species='32S16O')
 
 ## lower state
-lower = viblevel.VibLevel(name='lower',species='[32S][16O]')
-lower.add_level(name='X.3Σ+(v=0)', Tv=0, Bv=1, λv=1, γv=0.01)
+vlevel.add_manifold(name='X.3Σ+(v=0)', Tv=0, Bv=1, λv=1, γv=0.01)
 
 ## upper state
-upper = viblevel.VibLevel(name='upper',species='[32S][16O]')
-upper.add_level(name='B.3Π(v=0)', Tv=50000, Bv=1.1, Av=10) 
+vlevel.add_manifold(name='B.3Π(v=0)', Tv=50000, Bv=1.1, Av=10) 
 
-## transition
-transition = viblevel.VibLine('transition',upper,lower,J_l=range(31),)
-transition.add_transition_moment('B.3Π(v=0)','X.3Σ+(v=0)',μv=1)
-transition.construct()
+# ## vline
+vline = viblevel.Line(vlevel)
+vline.add_transition_moment('B.3Π(v=0)','X.3Σ+(v=0)',μv=1)
+vline.construct()
 
 ## save output
-transition.line['ν']
-transition.line.save('t0.psv')
+vline.line['ν']
+vline.line.save('t0.psv')
 
 ## plot
-qfig(1)
-lower.plot()
-title('Lower level energies')
-
-qfig(2)
-upper.plot()
-title('Upper level energies')
-
-qfig(3)
-transition.plot()
-title('Linestrengths by branch')
-
+vlevel.plot(fig=1)
+vline.plot(fig=2)
 show()
