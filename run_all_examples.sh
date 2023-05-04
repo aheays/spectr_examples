4d457ef1bd21f2320349fd4c5d4b6475c05aefd0
Alan Heays <aheays@fastmail.com>
2023-05-05 00:30:27 +1200

    run all models uncommented

--- run_all_examples.sh
+++ run_all_examples.sh
@@ -13,25 +13,25 @@ import matplotlib
 original_directory = os.getcwd()
 for path in (
 
-        # 'absorption/fit_absorption_1.py',
-        # 'absorption/fit_absorption_2.py',
-        # 'absorption/fit_absorption_3.py',
-        # 'absorption/fit_manually_1.py',
-        # 'absorption/fit_manually_2.py',
-        # 'absorption/fit_manually_3.py',
-        # 'absorption/fit_one_line.py',
+        'absorption/fit_absorption_1.py',
+        'absorption/fit_absorption_2.py',
+        'absorption/fit_absorption_3.py',
+        'absorption/fit_manually_1.py',
+        'absorption/fit_manually_2.py',
+        'absorption/fit_manually_3.py',
+        'absorption/fit_one_line.py',
 
-        # 'emission/compute_emission_band.py',
-        # 'emission/fit_emission_band_1.py',
-        # 'emission/fit_emission_band_2.py',
-        # 'emission/fit_emission_band_3.py',
+        'emission/compute_emission_band.py',
+        'emission/fit_emission_band_1.py',
+        'emission/fit_emission_band_2.py',
+        'emission/fit_emission_band_3.py',
 
-        # 'viblevel/3Π_3Σ+_transition.py',
-        # 'viblevel/run_comparison_with_pgopher.py',
+        'viblevel/3Π_3Σ+_transition.py',
+        'viblevel/run_comparison_with_pgopher.py',
 
         'argo/analyse_argo_1.py',
-        # 'argo/analyse_argo_2.py',
-        # 'argo/analyse_argo_3.py',
+        'argo/analyse_argo_2.py',
+        'argo/analyse_argo_3.py',
 
 ):
     print(f'\n\n===== running {path!r} =====')
